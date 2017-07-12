26365f4792aa3e89a5c7e87d004fb88ab7bb9110
John Daugherty <johndaugherty1986@gmail.com>
2017-07-12 17:43:29 -0500

    fixed action cable?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://johndaughertyportfolio.herokuapp.com', 'http://johndaughertyportfolio.herokuapp.com/']
+  config.action_cable.url = "wss://johndaughertyportfolio.herokuapp.com/cable"
 end
